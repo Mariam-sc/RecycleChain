4ec0b0b26605b3cefc2c3d37bf8206c383c125b0
Eiman <yungeimxn@gmail.com>
2020-12-22 19:39:50 +0400

    Update Tracking.sol

--- Tracking.sol
+++ Tracking.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.5.0; 
 
+ interface RegisterSC{
+       function getSellerSortingmMachineDetails(address addr) external view returns ( address [] memory); 
+    }
+    
+
 contract Tracking{
     
     //state variables - stored permanently in contract storage 
@@ -7,10 +12,30 @@ contract Tracking{
     address public plasticBottleAddress; // attained by scanning the QR code
     address public caller; 
     
+    
+    // ADD SOLD STATUS!!!!!!! ==> could be added in plastic bale SC
+    
     //event 
     event updateStatus(address plasticBottleAddress, string status, uint time); 
+    event debug(address [] tempArray);  
     
+    modifier sortingMachineOnly (address registerContractAddr, address sellerAddr){
+        
+        address[] memory tempArray; 
+
+       RegisterSC registerSC = RegisterSC(registerContractAddr); //pass contract address 
+       tempArray = registerSC.getSellerSortingmMachineDetails(sellerAddr); // pass address of sorting facility-seller
+       
+       emit debug(tempArray);
+       
+       for(uint256 i=0; i< tempArray.length; i++){ //only registered sorting machines can update the status of the bottle 
+           require(caller == tempArray[i],"Sorting machine is not registered.");
+           _;
+       }
+        
+   }
     
+
     //constructor - initilize state variables
     constructor() public{
         status = 'NoStatus'; 
@@ -24,26 +49,13 @@ contract Tracking{
     
     function updateStatusDisposed () public{
         status = 'disposed'; 
-    }
-    
-    function updateStatusSorted () public{
-        status = 'sorted'; 
-    }
-    
-    function announcEvent () public payable { //do you want this payable??
-        
         emit updateStatus(plasticBottleAddress, status, now); 
     }
     
-    
-  /* modifier sortingMachineOnly{
-       require()
-        
-   }
-    
-    modifier recyclerOnly{
-       require( )
-        
-   } */
+    function updateStatusSorted (address registerContractAddr, address sellerAddr) public sortingMachineOnly (registerContractAddr, sellerAddr){
+        status = 'sorted';
+        emit updateStatus(plasticBottleAddress, status, now); 
+       
+    }  
     
 }
