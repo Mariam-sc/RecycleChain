30ebd24fa3ca711eec5419fde07e47781722c531
Eiman <yungeimxn@gmail.com>
2020-12-27 18:04:36 +0400

    Update PlasticBale.sol

--- PlasticBale.sol
+++ PlasticBale.sol
@@ -10,15 +10,13 @@ contract PlasticBale{
     address[] public plasticBale; // retrived from plasticBaleCompleted event and passed through web3.js
     address [] public contributors; // retrived from plasticBaleCompleted event and passed through web3.js
   
-  struct bid{
-      bool open; 
-      uint highestBid; 
-      address payable highestBidder; 
-      uint startTime; 
-      uint endTime; 
-  }
-  
-  mapping(address=>bid) auction; 
+ // Bid variables 
+      bool public isOpen; 
+      uint public highestBid; 
+      address payable public highestBidder; 
+      uint public startTime; 
+      uint public endTime; 
+
   
   struct buyer{
       bool isExist; 
@@ -26,16 +24,16 @@ contract PlasticBale{
       uint deposit; 
   }
   
+  //Bidder[BuyerAddress]
   mapping(address=>buyer) bidder; 
   
   //Seller is the auctionOwner 
-   address payable auctionOwner;
+   address payable public auctionOwner;
    
    // Bidders are the Buyers 
    uint totalBidders; 
     
     constructor(address[] memory _plasticBale, address[] memory _contributors ) public {
-        
       plasticBale = _plasticBale; 
       contributors = _contributors; 
       auctionOwner = msg.sender; 
@@ -48,9 +46,9 @@ contract PlasticBale{
         _; 
     }
     
-    modifier onlyBidder(address registerContractAddr, address bidderAddr){
+    modifier onlyBidder(address registerContractAddr){
         RegisterSC registerSC = RegisterSC(registerContractAddr); //pass contract address 
-        require(registerSC.isBuyerExist(bidderAddr), "Bidder is not registered"); // might need to edit this to msg.sender instead bidderAddr
+        require(registerSC.isBuyerExist(msg.sender), "Bidder is not registered"); // might need to edit this to msg.sender instead bidderAddr
         _;                                                                        //when developing the Dapp 
     }
     
@@ -62,7 +60,7 @@ contract PlasticBale{
     event auctionEnded (address highestBidder, uint highestBid , uint closingTime); 
     
     
-    function addBidder(address registerContractAddr, address bidderAddr) onlyBidder(registerContractAddr, bidderAddr) public {
+    function addBidder(address registerContractAddr, address bidderAddr) onlyBidder (registerContractAddr) public {
         
     require(bidder[bidderAddr].isExist == false, "Bidder already joined the Auction.");
     totalBidders++; 
@@ -74,32 +72,31 @@ contract PlasticBale{
     
     function startAuction(uint closingTime, uint startPrice) onlyOwner payable public {
         
-        require(auction[msg.sender].open == false, "Auction is already open."); 
+        require(isOpen == false, "Auction is already open."); 
         
-        require(closingTime > now, "Auction time can only be set in future.");
+        require( closingTime > now, "Auction time can only be set in future.");
         
         require(totalBidders >= 2, "Not enough bidders are participating."); 
-        
-        require(msg.value >= startPrice || bidder[msg.sender].deposit >= startPrice, "Insufficient Deposit.."); 
-        
-        /*MIGHT BE WROOOOONG
-        bidder[msg.sender].deposit = msg.value;  */ 
-        
+    
         // address(0) is 0X00.. which is the genusis block 
-        auction[msg.sender] = bid(true, startPrice, address(0), now, closingTime); 
-        
+        isOpen = true; 
+        highestBid = startPrice; 
+        highestBidder = address(0); 
+        startTime = now; 
+        endTime = closingTime; 
+       
         emit auctionStarted(startPrice, closingTime); 
     }
     
-    function placeBid(address registerContractAddr,address bidderAddr, uint amount)  onlyBidder(registerContractAddr, bidderAddr) payable public{
+    function placeBid(address registerContractAddr, uint amount)  onlyBidder(registerContractAddr) payable public{
         
-        require(bidder[bidderAddr].isExist, "Buyer Address is not registered."); 
+        require(bidder[msg.sender].isExist, "Buyer Address is not registered."); 
         
-        //something could be wrong here - wrong address 
-        require(auction[msg.sender].open,"Auction is not opened.");
+        //something could be wrong here - wrong address (fixed but check)
+        require(isOpen,"Auction is not opened.");
         
         // To place a bid, amount sent has to be bigger than the highest bid 
-        require(amount > auction[msg.sender].highestBid, "Place a higher bid."); 
+        require(amount > highestBid, "Place a higher bid."); 
         
         //Validating the amount of wei sent with the transaction 
         require(msg.value == amount, "Insufficient Deposit."); 
@@ -107,48 +104,45 @@ contract PlasticBale{
         bidder[msg.sender].placedBids++; //STOPPED HERE WHEN CHECKING 
         bidder[msg.sender].deposit += msg.value; 
         
-        auction[bidderAddr].highestBid = amount; 
-        auction[bidderAddr].highestBidder= msg.sender; 
+        highestBid = amount; 
+        highestBidder= msg.sender; 
         
         emit bidPlaced(msg.sender, amount); 
         
     }
     
-    function exitAuction(address registerContractAddr, address bidderAddr) onlyBidder(registerContractAddr, bidderAddr) public {
+    function exitAuction(address registerContractAddr) onlyBidder(registerContractAddr) public {
         
+        // Buyers can exit auction if no bids are placed yet 
         require(bidder[msg.sender].placedBids == 0, "Buyer has placed a bid already."); 
-        refundBuyerDeposit(registerContractAddr, bidderAddr); 
         bidder[msg.sender] = buyer(false, 0 ,0); 
         totalBidders--; 
         emit bidderExited(msg.sender); 
-        
     }
     
-    function refundBuyerDeposit(address registerContractAddr, address bidderAddr) onlyBidder(registerContractAddr, bidderAddr) public{
-        (msg.sender).transfer(bidder[msg.sender].deposit); 
-        emit bidderRefunded(msg.sender, bidder[msg.sender].deposit); 
-        
-    }
     
     function endAuction() onlyOwner public{
         
-        require(auction[msg.sender].open, "Auction is not avalible.");
-        require(auction[msg.sender].endTime < now, "Auction duration is not up yet.");
-        require(auction[msg.sender].highestBidder != address(0), "No bids have been placed"); 
+        require( isOpen, "Auction is not avalible.");
+        require(endTime < now, "Auction duration is not up yet.");
+        require(highestBidder != address(0), "No bids have been placed"); 
         
         //close the bid
-        auction[msg.sender].open = false; 
+        isOpen = false; 
+        
         
         //Calculate shares 
         
-        uint halfAmount = auction[msg.sender].highestBid/2;
+        uint halfAmount = highestBid/2;
         
         // Pay the seller 
         (auctionOwner).transfer(halfAmount); 
         
         //Calculate each participants' share & reward recyclers 
-         
         
+        
+        emit auctionEnded(highestBidder, highestBid , now); 
+    
     }
     
     // For debugging 
